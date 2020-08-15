286fd6119183f3de04b3380fe7daae5b4784a497
srathina <mailtorsaravan@gmail.com>
2020-08-15 10:16:06 +0200

    refactored the gps header extraction using self written function

--- uart/UART_read.c
+++ uart/UART_read.c
@@ -9,23 +9,24 @@
 
 #define TIMEDELAY 100
 
+unsigned int get_temp_string(const unsigned char* , unsigned char* , unsigned int , char );
+
 int main ()
 {
   int serial_port ;
 
-  char gps;
-  int count;
-  char nmea_frame[200];
-  int token_cnt;
-
-  /*
-  ** Gps info strings
-  */
-  char lat[50];
-  char lng[50];
-  char Validity[10];
-  char ns[10];
-  char ew[10];
+  unsigned char gps;
+  unsigned int count;
+  unsigned char nmea_frame[100];
+  unsigned int comma_start;
+  unsigned char gps_header[15];
+  unsigned char lat[15];
+  unsigned char ns[5];
+  unsigned char lng[15];
+  unsigned char ew[5];
+  unsigned char utctime[15];
+  unsigned char datavalid[5];
+  unsigned char positionmode[5];
 
   system("sudo chmod 777 /dev/ttyS0");
 
@@ -41,86 +42,67 @@ int main ()
     return 1 ;
   }
 
-
-
   for (;;)
   {
 
       gps = serialGetchar(serial_port);
-
       /*
-      ** capture nmea frame which is between '$'
+      ** capture nmea frame which is between '$' and <CR><LF>
       */
       if (gps == '$')
       {
         count = 0;
-        gps = serialGetchar(serial_port);
-        nmea_frame[count] = gps;
-
-        while (gps != '$')
+        while (gps != '*') // <End character of data field>
         {
             gps = serialGetchar(serial_port);
-            nmea_frame[++count] = gps;
-
+            nmea_frame[count++] = gps;
          }
 
         nmea_frame[count] = '\0';
-        // printf("%s", nmea_frame);
 
         /*
-        ** separate the GPGLL frame
+        ** extract gps frame GPGLL
         */
-        char * token = strtok(nmea_frame, ",");
-
-        if ( strcmp(token,"GPGLL") == 0)
+        comma_start = get_temp_string(nmea_frame, gps_header, 0,',');
+        if (strcmp((char *)gps_header,"GPGLL") == 0)
         {
-            token_cnt = 0;
-
-            printf("\n");
-            // loop through the string to extract all other tokens
-            while( token != NULL )
-            {
-
-                /*
-                ** Extract and print gps info...
-                */
-                if(token_cnt == 1)
-                {
-                    strcpy(lat,token);
-                    printf("Latitude: %s\n",lat);
-                }
-                else if(token_cnt == 3)
-                {
-                    strcpy(lng,token);
-                    printf("Longitude: %s\n",lng);
-                }
-                else if(token_cnt == 6)
-                {
-                    strcpy(Validity,token);
-                    printf("Validity: %s\n",Validity);
-                }
-                else if(token_cnt == 2)
-                {
-                    strcpy(ns,token);
-                    printf("N/S: %s\n",ns);
-                }
-                else if(token_cnt == 4)
-                {
-                    strcpy(ew,token);
-                    printf("E/W: %s\n",ew);
-                }
-                token_cnt++;
-
-                //printf( " %s\n", token ); //printing each token
-                token = strtok(NULL, ",");
-            }
-
+          comma_start = 5;
+          comma_start = get_temp_string(nmea_frame, lat, comma_start+1,',');
+          comma_start = get_temp_string(nmea_frame, ns, comma_start+1,',');
+          comma_start = get_temp_string(nmea_frame, lng, comma_start+1,',');
+          comma_start = get_temp_string(nmea_frame, ew, comma_start+1,',');
+          comma_start = get_temp_string(nmea_frame, utctime, comma_start+1,',');
+          comma_start = get_temp_string(nmea_frame, datavalid, comma_start+1,',');
+          comma_start = get_temp_string(nmea_frame, positionmode, comma_start+1,'*');
+          printf("%s\n",gps_header);
+          printf("%s\n",lat);
+          printf("%s\n",ns);
+          printf("%s\n",lng);
+          printf("%s\n",ew);
+          printf("%s\n",utctime);
+          printf("%s\n",datavalid);
+          printf("%s\n",positionmode);
+          printf("\n");
         }
-        fflush (stdout);
+
       }
 
   }
 
-
 }
 
+unsigned int get_temp_string(const unsigned char* src_str, unsigned char* dest_str, unsigned int start, char delimit)
+{
+
+        unsigned int j = 0;
+
+        while (src_str[start] != delimit)
+        {
+            dest_str[j] = src_str[start];
+            start++;
+            j++;
+        }
+        dest_str[j] = '\0';
+        return(start);
+
+}
